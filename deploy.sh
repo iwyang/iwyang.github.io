d00bf7f0cee23ab7e54162c6911a0626ccf4bad1
iwyang <455343442@qq.com>
2025-12-16 19:19:24 +0800

    site backup

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo -e "\033[0;32mDeploying updates to gitee...\033[0m"
+echo -e "\033[0;32mDeploying updates to vps...\033[0m"
 git add .	
 git commit -m "site backup"
 git push --force origin backup
